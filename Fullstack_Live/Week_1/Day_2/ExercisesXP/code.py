6d379d0fff251200f183f6dcf5c95950b6e39871
Jonathan K. <noudas1@gmail.com>
2024-11-04 10:08:53 -0300

    Week 1 - Day 2 fix

--- Fullstack_Live/Week_1/Day_2/ExercisesXP/code.py
+++ Fullstack_Live/Week_1/Day_2/ExercisesXP/code.py
@@ -228,13 +228,13 @@ def exercise_10():
         
 
 
-#exercise_1()
-#exercise_2()
-#exercise_3()
-#exercise_4()
-#exercise_5()
-#exercise_6()
-#exercise_7()
-#exercise_8()
-#exercise_9()
+exercise_1()
+exercise_2()
+exercise_3()
+exercise_4()
+exercise_5()
+exercise_6()
+exercise_7()
+exercise_8()
+exercise_9()
 exercise_10()
