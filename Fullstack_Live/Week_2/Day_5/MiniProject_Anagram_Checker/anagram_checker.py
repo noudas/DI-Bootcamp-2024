080b4e41c7ee947c4f0c6c567469177a3ff7e573
Jonathan K. <noudas1@gmail.com>
2024-11-14 06:39:21 -0300

    Week 2 - Day 5

---
+++ Fullstack_Live/Week_2/Day_5/MiniProject_Anagram_Checker/anagram_checker.py
@@ -0,0 +1,42 @@
+import json
+
+# Create a new file called anagram_checker.py which contains a class called AnagramChecker.
+# 
+# The class should have the following methods:
+class AnagramChecker:
+    # __init__ - should load the word list file (text file) into a variable, so that it can be searched later on in the code.
+    def __init__(self):
+        with open("list_words.txt" , "r", encoding="utf-8") as f:
+            self.list = set(word.strip() for word in f)
+    
+# is_valid_word(word) – should check if the given word (ie. the word of the user) is a valid word.
+    def is_valid_word(self,word):
+        if isinstance(word,str) and word.isalpha():
+            return True
+        else:
+            raise TypeError(f"{word} is not a word!")
+
+# Only a single word is allowed. If the user typed more than one word, show an error message. (Hint: how do we know how many words were typed?)
+    def only_one_word(self, word):
+        if ' ' in word.strip():
+            raise ValueError("Only one word is allowed.")
+        return True
+
+# Hint: you might want to create a separate method called is_anagram(word1, word2), that will compare 2 words and return True if they contain the same letters (but not in the same order), and False if not.
+# get_anagrams(word) – should find all anagrams for the given word. 
+    def get_anagrams(self,word):
+        word = word.strip()
+        if self.is_valid_word(word) and self.only_one_word(word):
+            word = word.lower()
+            list_anagram = [ana for ana in self.list if len(ana) == len(word) and sorted(ana.lower()) == sorted(word)]
+            return list_anagram
+
+# (eg. if word of the user is ‘meat’, the function should return a list containing [“mate”, “tame”, “team”].)
+# 
+# 
+# Note: None of the methods in the class should print anything.
+# 
+# Now create another Python file, called anagrams.py. 
+
+# test = AnagramChecker()
+# test.get_anagrams("Meat")
---
+++ Fullstack_Live/Week_2/Day_5/MiniProject_Anagram_Checker/anagrams.py
@@ -0,0 +1,91 @@
+from anagram_checker import AnagramChecker
+
+# This will contain all the UI (user interface) functionality of your program, and will rely on AnagramChecker for the anagram-related logic.
+# 
+# It should do the following:
+# Show a menu, offering the user to input a word or exit. Keep showing the menu until the user chooses to exit.
+def loadchecker():
+    return AnagramChecker()
+
+def menu():
+    try:
+        print(r"""
+ _   _ _ _   _                 _               
+| | | | | |_(_)_ __ ___   __ _| |_ ___         
+| | | | | __| | '_ ` _ \ / _` | __/ _ \        
+| |_| | | |_| | | | | | | (_| | ||  __/        
+ \___/|_|\__|_|_| |_| |_|\__,_|\__\___|        
+  __ _ _ __   __ _  __ _ _ __ __ _ _ __ ___    
+ / _` | '_ \ / _` |/ _` | '__/ _` | '_ ` _ \   
+| (_| | | | | (_| | (_| | | | (_| | | | | | |  
+ \__,_|_| |_|\__,_|\__, |_|  \__,_|_| |_| |_|  
+  __ _  ___ _ __   |___/ __ __ _| |_ ___  _ __ 
+ / _` |/ _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
+| (_| |  __/ | | |  __/ | | (_| | || (_) | |   
+ \__, |\___|_| |_|\___|_|  \__,_|\__\___/|_|   
+ |___/                                         """)
+    
+        print("""\n\nDo you wish to:
+(1). Type a Word
+(2). Exit""")
+    
+# If the user chooses to input a word, it must be accepted from the user’s keyboard input, and then be validated:
+        answer = int(input("Choose: "))
+        return answer
+    except:
+        raise TypeError("Not a number")
+    
+
+# Only alphabetic characters are allowed. No numbers or special characters.
+# Whitespace should be removed from the start and end of the user’s input.
+ 
+# Once your code has decided that the user’s input is valid, it should find out the following:
+
+def main():
+    # Ensure an instance of AnagramChecker is created
+    anagramchecker = loadchecker()
+
+    while True:
+        choice = menu()
+
+        try:
+            match choice:
+                case 1:
+                    print("""Rules:
+                          
+1. No more than one word
+2. No numbers or special characters
+3. You have to be amazed
+                          """)
+                    
+                    word = input("Type your word: ").strip().upper()
+                    
+                    anagram_list = anagramchecker.get_anagrams(word)
+                    
+                    if anagram_list == []:
+                        print("Sorry! But this is not an anagramable word! Try another")
+                    else:
+                        print(f"\nYour word was '{word}'")
+                        print(f"Anagrams for your word: {', '.join(anagram_list)}")
+
+                case 2:
+                    print("Goodbye!")
+                    break
+
+                case _:
+                    print("Invalid choice, please try again.")
+        except:
+            TypeError("Not a number")
+
+main()
+
+main()
+
+# All possible anagrams to the user’s word.
+# Create an AnagramChecker instance and apply it to the steps created above.
+# Display the information about the word in a user-friendly, nicely-formatted message such as:
+# 
+# 
+# YOUR WORD :”MEAT”
+# this is a valid English word.
+# Anagrams for your word: mate, tame, team.
