dcb3b92b5ab5955e1e4bf08dc6931d0b47d73838
Jonathan K. <noudas1@gmail.com>
2024-11-25 11:55:56 -0300

    Week 4 - Day 1

--- Fullstack_Live/Week_4/Day_1/ExercisesXP/exercise6.js
+++ Fullstack_Live/Week_4/Day_1/ExercisesXP/exercise6.js
@@ -5,3 +5,10 @@ const details = {
   }
   
 // Given the object above and using a for loop, console.log “my name is Rudolf the reindeer”
+
+let stringy = '';
+
+for (let key in details){
+    stringy += key + ' ' + details[key] + ' '
+}
+console.log(stringy);
--- Fullstack_Live/Week_4/Day_1/ExercisesXP/exercise7.js
+++ Fullstack_Live/Week_4/Day_1/ExercisesXP/exercise7.js
@@ -2,6 +2,15 @@ const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
 
 // A group of friends have decided to start a secret society. 
 // The society’s name will be the first letter of each of their names sorted in alphabetical order.
+let names_sorted = names.sort()
+console.log(names_sorted);
+
+let secret_name = ''
+
+for (let name in names_sorted){
+    secret_name += names_sorted[name].charAt(0)
+}
+console.log(secret_name.toUpperCase());
 
 // Hint: a string is an array of letters
 // Console.log the name of their secret society. The output should be “ABJKPS”
