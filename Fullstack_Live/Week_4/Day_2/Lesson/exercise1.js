0336922832c0e02a15dc92c28dc0a0e8c4000412
Jonathan K. <noudas1@gmail.com>
2024-11-26 06:34:25 -0300

    Week 4 - Day 2

--- Fullstack_Live/Week_4/Day_2/Lesson/exercise1.js
+++ Fullstack_Live/Week_4/Day_2/Lesson/exercise1.js
@@ -1,7 +1,16 @@
 // For each of the questions, find 2 WAYS of accessing :
 
 // 1. The div DOM node?
+const divDOMNode1 = document.body.firstElementChild;
+const divDOMNode2 = document.body.children[0];
 
 // 2. The ul DOM node?
+const ulDOMNode1 = document.body.children[1];
+const ulDOMNode2 = document.body.firstElementChild.nextElementSibling;
 
 // 3. The second li (with Pete)?
+const ilDOMNode1 = document.body.children[1].children[1];
+const ilDOMNode2 = document.body.lastElementChild;
+
+console.log(ilDOMNode1);
+console.log(ilDOMNode2);
