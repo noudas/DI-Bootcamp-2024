f9cd3241a10d59ab6f630fb9c1025fd1447bc976
Jonathan K. <noudas1@gmail.com>
2024-11-29 10:43:32 -0300

    Week 4 - Day 4

---
+++ Fullstack_Live/Week_4/Day_4/MiniProject#2/index.js
@@ -0,0 +1,13 @@
+// Goal of this Mini project
+// This looks complex, but it’s pretty simple.
+
+// Get the clicked element, or key press.
+// Use it’s value to get the correct audio element.
+// Play the audio element.
+
+// Hints
+// Focus on the JS. Get it working. Make it look pretty later.
+// Look up how to play audio in HTML and JS. (w3schools)
+// Add event listeners for keyboard and/or mouse clicks.
+// for keyboard events, you can get the key by looking at the property keyCode of the event object
+// for mouse click events, you can get the key by looking at the this object.
