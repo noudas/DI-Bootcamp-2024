1d0b66a8f08fd03f5e983d1db8af9336cc93b63b
Jonathan K. <noudas1@gmail.com>
2024-12-04 13:45:41 -0300

    Week 5 - Day 4

--- Fullstack_Live/Week_5/Day_4/MiniProject/index.js
+++ Fullstack_Live/Week_5/Day_4/MiniProject/index.js
@@ -6,69 +6,91 @@
 // The id must start at 0, and is incremented for every new quote. So the first quote will have the id 0, the second quote the id 1, the third quote the id 2 ect…
 
 // Populate the array with a few quotes that you like.
-let arrayObjects = []
+function arrayGenerator() {
+        let arrayObjects = []
 
-// Add quotes from Jack Black
-arrayObjects.push({
-    id: 0,
-    author: "Jack Black",
-    quote: "You can't make a good movie without a good script. That's a no-brainer."
-});
-arrayObjects.push({
-    id: 1,
-    author: "Jack Black",
-    quote: "My dream is to make movies with my friends."
-});
+        // Add quotes from Jack Black
+        arrayObjects.push({
+            id: 0,
+            author: "Jack Black",
+            quote: "You can't make a good movie without a good script. That's a no-brainer."
+        });
+        arrayObjects.push({
+            id: 1,
+            author: "Jack Black",
+            quote: "My dream is to make movies with my friends."
+        });
 
-// Add quotes from Monty Python
-arrayObjects.push({
-    id: 2,
-    author: "Monty Python",
-    quote: "I'm not dead yet!"
-});
-arrayObjects.push({
-    id: 3,
-    author: "Monty Python",
-    quote: "It's just a flesh wound!"
-});
-arrayObjects.push({
-    id: 4,
-    author: "Monty Python",
-    quote: "We are the knights who say... Ni!"
-});
+        // Add quotes from Monty Python
+        arrayObjects.push({
+            id: 2,
+            author: "Monty Python",
+            quote: "I'm not dead yet!"
+        });
+        arrayObjects.push({
+            id: 3,
+            author: "Monty Python",
+            quote: "It's just a flesh wound!"
+        });
+        arrayObjects.push({
+            id: 4,
+            author: "Monty Python",
+            quote: "We are the knights who say... Ni!"
+        });
 
-// Add quotes from Malcolm Gladwell
-arrayObjects.push({
-    id: 5,
-    author: "Malcolm Gladwell",
-    quote: "The key to good decision making is not knowledge. It is understanding."
-});
-arrayObjects.push({
-    id: 6,
-    author: "Malcolm Gladwell",
-    quote: "Success is not a random act. It arises out of a predictable and powerful set of circumstances and opportunities."
-});
-arrayObjects.push({
-    id: 7,
-    author: "Malcolm Gladwell",
-    quote: "We learn by example and by direct experience because there are real limits to the adequacy of verbal instruction."
-});
-
+        // Add quotes from Malcolm Gladwell
+        arrayObjects.push({
+            id: 5,
+            author: "Malcolm Gladwell",
+            quote: "The key to good decision making is not knowledge. It is understanding."
+        });
+        arrayObjects.push({
+            id: 6,
+            author: "Malcolm Gladwell",
+            quote: "Success is not a random act. It arises out of a predictable and powerful set of circumstances and opportunities."
+        });
+        arrayObjects.push({
+            id: 7,
+            author: "Malcolm Gladwell",
+            quote: "We learn by example and by direct experience because there are real limits to the adequacy of verbal instruction."
+        });
+    return arrayObjects
+}
 
 // When the “Generate Quote” button is pressed, retrieve randomly a quote (ie. an object), and display it in the DOM - like the image above.
 // Important: When clicking on the button, make sure you don’t display the same quote twice in a row.
 let quoteSection = document.getElementById("quoteSection")
 let quotePrintArea = document.getElementById("quotePrint")
 let quoteGeneratorButton = document.getElementById("generateQuote")
+let formGeneratorButton = document.getElementById("formGenerator")
 
 function randomQuote() {
+    let arrayObjects = arrayGenerator()
     let randomIndex = Math.floor(Math.random() * arrayObjects.length);
     return arrayObjects[randomIndex]
 }
 
+function formGenerator() {
+    let generatorDOMmodule = document.getElementById("Generator")
+    let quotePrint = document.getElementById("quotePrint")
+    quotePrint.style.visibility = "hidden";
+    let h1DOMmodule = document.getElementsByTagName("h1")[0];
+    h1DOMmodule.textContent = "Add your Quote"
+
+}
+
+formGeneratorButton.addEventListener("click",(event) =>{
+    event.preventDefault();
+    formGenerator()
+})
+
 quoteGeneratorButton.addEventListener("click", (event) => {
     event.preventDefault();
     let quote = randomQuote();
+    let h1DOMmodule = document.getElementsByTagName("h1")[0];
+    h1DOMmodule.textContent = "Quote Generator"
+    let quotePrint = document.getElementById("quotePrint")
+    quotePrint.style.visibility = "";
 
     let existingParagraph = document.querySelector("#quoteDisplay");
     let existingAuthor = document.querySelector("#quoteAuthor");
@@ -92,7 +114,8 @@ quoteGeneratorButton.addEventListener("click", (event) => {
 
 
 // Part 2 : Add buttons
-// In the HTML file, create a form with the inputs “Quote” and “Author” and a button. So when you click on the button, you can add a new quote to the array of object.
+// In the HTML file, create a form with the inputs “Quote” and “Author” and a button. 
+// So when you click on the button, you can add a new quote to the array of object.
 // Important: Don’t forget to set the id of the new quotes
 
 // In the HTML file, under the displayed quote, create a few more buttons:
