cf3840507d1d19db48fd4da777f79e71bfc715ad
Jonathan K. <noudas1@gmail.com>
2024-12-04 08:21:04 -0300

    Week 5 - Day 4

---
+++ Fullstack_Live/Week_5/Day_4/ExerciseXP/exercise1.js
@@ -0,0 +1,8 @@
+// Create a form like the one above. The form should contain three inputs:
+// a small text input asking for a name,
+// a larger textarea input to write a message,
+// a submit input (“Send”)
+
+// When you click the Send button, the form will be submitted with a GET method. (you can use the same HTML file for the action url)
+// Where will the sent data appear?
+// Nowhere because there is no server, but it will be stored at the end of the URL as URL parameters, appended as a query string.
---
+++ Fullstack_Live/Week_5/Day_4/ExerciseXP/exercise2.js
@@ -0,0 +1,7 @@
+// Use the same form structure as Exercise 1.
+// When you click the Send button, the form will be submitted with a POST method.(you can use the same HTML file for the action url)
+// Where will the sent data appear? Hint : Look at the Network Tab
+
+// Nowhere because there is no server. Unlike a GET request, the data will not appear in the URL as query parameters 
+// but will be sent in the request body. 
+// You can inspect this data in the Network tab of the browser's developer tools.
