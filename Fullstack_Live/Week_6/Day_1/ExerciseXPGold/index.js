fa2886eb54f044906ef2075da9004b3dcc6d3493
Jonathan K. <noudas1@gmail.com>
2024-12-08 11:17:33 -0300

    Week 6 - Day 1

--- Fullstack_Live/Week_6/Day_1/ExerciseXPGold/index.js
+++ Fullstack_Live/Week_6/Day_1/ExerciseXPGold/index.js
@@ -5,11 +5,19 @@
 // If any of the promises are rejected, the function should catch them.
 // Explain in a comment how Promise.all work and why you receive this output.
 
-// const promise1 = Promise.resolve(3);
-// const promise2 = 42;
-// const promise3 = new Promise((resolve, reject) => {
-//   setTimeout(resolve, 3000, 'foo');
-// });
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 3000, 'foo');
+});
+Promise.all([promise1, promise2, promise3])
+  .then(result => {
+    console.log(result); // expected output: [3, 42, "foo"]
+  })
+  .catch(error => {
+    console.log(error); // Catch any errors if any promise rejects
+  });
+
 // // expected output: Array [3, 42, "foo"]
 
 
