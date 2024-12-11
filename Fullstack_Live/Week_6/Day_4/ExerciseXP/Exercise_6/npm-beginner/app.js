89531b3cf840e1f1c114946cfb4097a49b8ecf4d
Jonathan K. <noudas1@gmail.com>
2024-12-11 17:12:40 -0300

    Week 6 - Day 4

---
+++ Fullstack_Live/Week_6/Day_4/ExerciseXP/Exercise_6/npm-beginner/app.js
@@ -0,0 +1,19 @@
+// 🌟 Exercise 6: Simple NPM Package Usage
+// Instructions
+// Create a directory named npm-beginner.
+
+// Inside the npm-beginner directory, open a terminal and run npm init to initialize a new Node.js project. Follow the prompts to set up your project.
+
+// Install the chalk package, which provides easy coloring for terminal output, by running npm install chalk in the terminal.
+
+// Create a file named app.js inside the npm-beginner directory.
+
+// In app.js, require the chalk package and use it to color and style text in the terminal.
+import chalk from "chalk";
+
+// Write a simple script that uses chalk to print a colorful message.
+  
+console.log(chalk.red("R") + chalk.magenta("A") + chalk.yellow("I") + chalk.green("N") + chalk.cyan("B") + chalk.blue("O") + chalk.red("W"));
+
+
+// Run app.js using Node.js and observe the colorful output in the terminal.
