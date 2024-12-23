da9c7b31e21cc631e17ec45847b008a75258df36
Jonathan K. <noudas1@gmail.com>
2024-12-23 11:42:43 -0300

    Week 8 - Day 2

--- Fullstack_Live/Week_8/Day_2/ExerciseXP/vite-project/src/components/Car.jsx
+++ Fullstack_Live/Week_8/Day_2/ExerciseXP/vite-project/src/components/Car.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import Garage from "./Garage";
+
 const CarModel = (props) => {
+    const [color, setColor] = useState("Silver");
+
+    const toggleColor = () => {
+        setColor((prevColor) => (prevColor === "Silver" ? "Puke Green" : "Silver"));
+    };
+
     return (
         <>
             <header>
                 <h3>The brand is {props.name}</h3>
-                <h3>This car is {props.model}</h3>
+                <h3>This car is {props.model} and {color}</h3>
+                <button onClick={toggleColor}>Change car color</button>
+                <Garage size="small"/>
             </header>
         </>
     );
---
+++ Fullstack_Live/Week_8/Day_2/ExerciseXP/vite-project/src/components/Garage.jsx
@@ -0,0 +1,9 @@
+const Garage = (props) => {
+    return (
+        <div>
+            <h3>Who lives in my {props.size} Garage?</h3>
+        </div>
+    );
+};
+
+export default Garage;
