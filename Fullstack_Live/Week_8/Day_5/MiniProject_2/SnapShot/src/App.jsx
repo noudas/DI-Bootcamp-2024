517bd4714ced48b6ca618d1911472bf9dfaa8015
Jonathan K. <noudas1@gmail.com>
2024-12-26 14:21:54 -0300

    Week 8 - Day 5

--- Fullstack_Live/Week_8/Day_5/MiniProject_2/SnapShot/src/App.jsx
+++ Fullstack_Live/Week_8/Day_5/MiniProject_2/SnapShot/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import SearchBar from './components/SearchBar'
 
 function App() {
 
   return (
     <>
-
+      <SearchBar/>
     </>
   )
 }
---
+++ Fullstack_Live/Week_8/Day_5/MiniProject_2/SnapShot/src/components/ImageArea.jsx
@@ -0,0 +1,7 @@
+const ImageArea = (props) =>{
+    const {title} = props.info;
+    return(
+        <h3>{title} Pictures</h3>
+        
+    )
+}
