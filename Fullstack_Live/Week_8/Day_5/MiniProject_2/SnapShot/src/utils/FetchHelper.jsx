c7c8ef3b5cac5755b85ba1e7d8c672920acd8b78
Jonathan K. <noudas1@gmail.com>
2024-12-26 14:39:22 -0300

    Week 8 - Day 5

---
+++ Fullstack_Live/Week_8/Day_5/MiniProject_2/SnapShot/src/utils/FetchHelper.jsx
@@ -0,0 +1,26 @@
+const FetchHelper = async (query, perPage = 30) =>{
+    const API_KEY = ENV_API_KEY
+    const BASE_URL = "https://api.pexels.com/v1/search";
+
+    try {
+        const response = await fetch(`${BASE_URL}?query=$${query}&per_page=${perPage}`,{
+            headers:{
+                Authorization:API_KEY,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const data = response.json();
+        return data.photo
+
+    } catch (error) {
+        
+        console.error("FetchHelper Error:", error);
+        throw error;
+    }
+}
+
+export default FetchHelper
