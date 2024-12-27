6cdc581f22867091729c68a0ecb4c5de7aad5ee0
Jonathan K. <noudas1@gmail.com>
2024-12-27 14:00:27 -0300

    Week 8 - Day 5

--- Fullstack_Live/Week_8/Day_5/MiniProject_2/SnapShot/src/App.jsx
+++ Fullstack_Live/Week_8/Day_5/MiniProject_2/SnapShot/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Routes, Route, BrowserRouter as Router } from 'react-router'; // Import Router from react-router
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router';
 import './App.css';
 import ImageArea from './components/ImageArea';
 import SearchBar from './components/SearchBar';
@@ -8,24 +8,36 @@ import Navigation from './components/Navigation';
 function App() {
   const [query, setQuery] = useState('Random');
 
-  const handleSearch = (searchQuery) => {
-    setQuery(searchQuery);
-  };
-
   return (
     <Router>
-      <SearchBar onSearch={handleSearch} />
+      <SearchBar onSearch={setQuery} />
       <Navigation />
-
-      <Routes>
-        <Route path="/Mountain" element={<ImageArea query="Mountain" />} />
-        <Route path="/Beaches" element={<ImageArea query="Beaches" />} />
-        <Route path="/Birds" element={<ImageArea query="Birds" />} />
-        <Route path="/Food" element={<ImageArea query="Food" />} />
-        <Route path="/" element={<ImageArea query={query} />} />
-        <Route path="*" element={<h1>404 Page not found</h1>}/>
-      </Routes>
+      <RoutesWrapper query={query} setQuery={setQuery} />
     </Router>
+  );
+}
+
+function RoutesWrapper({ query, setQuery }) {
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const searchParam = params.get('search');
+    if (searchParam) {
+      setQuery(searchParam);
+    }
+  }, [location.search, setQuery]);
+
+  return (
+    <Routes>
+      <Route path="/Mountain" element={<ImageArea query="Mountain" />} />
+      <Route path="/Beaches" element={<ImageArea query="Beaches" />} />
+      <Route path="/Birds" element={<ImageArea query="Birds" />} />
+      <Route path="/Food" element={<ImageArea query="Food" />} />
+      <Route path="/search/:query" element={<ImageArea query={query} />} />
+      <Route path="/" element={<ImageArea query={query} />} />
+      <Route path="*" element={<h1>404 Page not found</h1>} />
+    </Routes>
   );
 }
 
