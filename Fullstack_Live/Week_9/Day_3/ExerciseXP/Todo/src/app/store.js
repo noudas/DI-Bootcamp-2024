6bc3cc853dc588f4410281c4249cb2b00b30ef46
Jonathan K. <noudas1@gmail.com>
2024-12-31 09:47:12 -0300

    Week 9 - Day 3

--- Fullstack_Live/Week_9/Day_3/ExerciseXP/Todo/src/app/store.js
+++ Fullstack_Live/Week_9/Day_3/ExerciseXP/Todo/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import todoReducer from "./TodoSlice";
+import todoReducer from "../features/Todo/TodoSlice";
 
 const appReducer = combineReducers({
     todos: todoReducer,
