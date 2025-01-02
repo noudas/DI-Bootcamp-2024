beeae2de2bd865c699383828eba85d9fae258863
Jonathan K. <noudas1@gmail.com>
2025-01-02 10:46:57 -0300

    Week 9 - Day 5

--- Fullstack_Live/Week_9/Day_4/ExerciseXP/vite-project/src/redux/DataSlice.js
+++ Fullstack_Live/Week_9/Day_4/ExerciseXP/vite-project/src/redux/DataSlice.js
@@ -6,11 +6,38 @@ const initialState = {
     error:null
 }
 
+export const fetchData = createAsyncThunk('data/fetchData', async (state, action) =>{
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        if (!response.ok){
+            throw new Error("failed to Fecth data")
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+    }
+});
+
 const DataSlice = createSlice({
     name:'data',
     initialState,
     reducers:{},
-    extraReducers(builder){}
+    extraReducers(builder){
+        builder
+        .addCase(fetchData.pending, (state) =>{
+            state.status = 'pending';
+            state.error = null;
+        })
+        .addCase(fetchData.fulfilled, (state, action) =>{
+                    state.status = "success";
+                    state.todos = action.payload;
+        })
+        .addCase(fetchData.rejected, (state, action) =>{
+                    state.status = "failed";
+                    state.error = action.payload;
+        })
+    }
 });
 
 export const {} = DataSlice.actions;
