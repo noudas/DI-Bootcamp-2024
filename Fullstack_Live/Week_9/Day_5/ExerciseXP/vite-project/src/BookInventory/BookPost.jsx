24c1db326d8e43fd99a3e98e30d767de5f9187e9
Jonathan K. <noudas1@gmail.com>
2025-01-08 11:00:50 -0300

    Week 10 - Day 4

--- Fullstack_Live/Week_9/Day_5/ExerciseXP/vite-project/src/BookInventory/BookPost.jsx
+++ Fullstack_Live/Week_9/Day_5/ExerciseXP/vite-project/src/BookInventory/BookPost.jsx
@@ -1,8 +1,48 @@
+import { useRef } from "react"
+import { useAppDispatch } from "./hooks";
+import { addBook } from "./state/slice";
+
 const BookPost = (props) =>{
+    const dispatch = useAppDispatch();
+    const refTitle = useRef();
+    const refAuthor = useRef();
+    const refGenre = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const title = refTitle.current.value;
+        const author = refAuthor.current.value;
+        const genre = refGenre.current.value;
+
+        if (title && author && genre) {
+            dispatch(addBook({ title, author, genre }));
+            refTitle.current.value = "";
+            refAuthor.current.value = "";
+            refGenre.current.value = "";
+        }
+    };
+
 return (
-    <>
-    </>
-)
-}
+        <>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Title:</label>
+                    <input id="title" type="text" ref={refTitle} required />
+                </div>
+                <div>
+                    <label htmlFor="author">Author:</label>
+                    <input id="author" type="text" ref={refAuthor} required />
+                </div>
+                <div>
+                    <label htmlFor="genre">Genre:</label>
+                    <input id="genre" type="text" ref={refGenre} required />
+                </div>
+                <button type="submit">Add Book</button>
+            </form>
+        </>
+    );
+
+};
 
 export default BookPost
