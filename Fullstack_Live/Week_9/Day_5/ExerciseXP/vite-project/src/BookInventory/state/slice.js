5df44671a37bd6f639809b49c5ea564351793826
Jonathan K. <noudas1@gmail.com>
2025-01-08 10:24:00 -0300

    Week 10 - Day 4

--- Fullstack_Live/Week_9/Day_5/ExerciseXP/vite-project/src/BookInventory/state/slice.js
+++ Fullstack_Live/Week_9/Day_5/ExerciseXP/vite-project/src/BookInventory/state/slice.js
@@ -19,10 +19,22 @@ const booksSlice = createSlice({
             };
             state.books.push(newBook);
         },
+        selectBooks: (state) =>{
+            return state.books;
+        },
+        selectHorrorBooks: (state) =>{
+            return state.books.filter((book) => book.genre.toLowerCase() === 'horror');
+        },
+        selectFantasyBooks: (state, action) =>{
+            return state.books.filter((book) => book.genre.toLowerCase() === 'fantasy');
+        },
+        selectScienceFictionBooks: (state, action) =>{
+            return state.books.filter((book) => book.genre.toLowerCase() === 'scify');
+        },
     },
     extraReducers(builder){},
 });
 
 export const stateReducer = (state) => state.bookReducer;
-export const {addBook} = booksSlice.actions;
+export const {addBook, selectBooks, selectHorrorBooks, selectFantasyBooks, selectScienceFictionBooks} = booksSlice.actions;
 export default booksSlice.reducer;
