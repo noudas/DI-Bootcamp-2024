5f82a6c99a9b749994d5881570d5c21a7139d2e3
Jonathan K. <noudas1@gmail.com>
2025-01-13 17:31:45 -0300

    Week 11 - Day 1

--- Fullstack_Live/Week_11/Day_1/ExerciseXP/app.js
+++ Fullstack_Live/Week_11/Day_1/ExerciseXP/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+// const db = require("./config/db");
+const userRouter = require("./routes/userRoute");
+const userController = require("./controllers/userController");
 
 const app = express();
 
@@ -17,4 +20,16 @@ const {PORT} = process.env;
 
 app.listen(PORT || 5000, () =>{
     console.log(`run on ${PORT || 5000}`);
-});
+    
+})
+
+app.use('/api/user', userRouter);
+// async function testConnection() {
+//     try {
+//       const response = await db.raw("select version()");
+//       console.log(response.rows);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// testConnection()
---
+++ Fullstack_Live/Week_11/Day_1/ExerciseXP/controllers/userController.js
@@ -0,0 +1,106 @@
+const userModel = require('../models/userModel');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = {
+    registerUser:  async (req, res) =>{
+        const {password, email} = req.body;
+        
+        try {
+            const user = await userModel.createUser(password, email);
+            
+            res.status(201).json({
+                message: "User registered successfully",
+                user,
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message:"Internal Error"
+            });
+        }
+    },
+    loginUser: async (req, res) => {
+        const {password, email} = req.body;
+
+        try {
+            const user = await userModel.getUserByEmail(email);
+            if(!user){
+                res.status(404).json({message:"User not found"})
+            }
+            
+            const matchinPassword = await bcrypt.compare(password + '', user.password);
+
+            if(!matchinPassword){
+                res.status(404).json({message:"Wrong Password"})
+            }
+
+            const {ACCESS_TOKEN_SECRET} = process.env;
+            /* Generate a token */
+            const acessToken = jwt.sign(
+                {userid: user.id, email: user.email},
+                ACCESS_TOKEN_SECRET,
+                { expiresIn: "60s" }
+            );
+
+            /* set cookie to the token via http*/
+            res.cookie("Token_Cookie", acessToken, {
+                maxAge: 60 * 1000,
+                httpOnly: true,
+            });
+
+            /* Response to client */
+            res.status(200).json({ message: "Login successfully",
+                user: {userid: user.id, email: user.email},
+                token:acessToken,
+             });
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message:"Internal Error"
+            });
+        }
+    },
+
+    getUsers: async (req, res) => {
+        try {
+            const users = await userModel.getUsers();
+            res.json(users)
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message:"Internal Error"
+            });
+        }
+
+    },
+    logoutUser:async (req, res) =>{
+        req.clearCookie("Token_Cookie");
+        req.cookies.token = null;
+        delete req.cookies.token;
+        /*Set null in db collumn*/
+        res.sendStatus(200);
+    },
+    verifyAuth: async (req,res) =>{
+        const { userid, email}  = req.user;
+        const {ACCESS_TOKEN_SECRET} = process.env;
+    
+        const newAccessToken= jwt.sign(
+            { userid, email},
+            ACCESS_TOKEN_SECRET,
+            {expiresIn: "60s"}
+        );
+        res.cookie("Token_Cookie", newAccessToken), {
+            httpOnly: true,
+            maxAge: 60 *1000,
+        }
+        res.json({
+                message: "verified",
+                user:{userid, email},
+                token: newAccessToken,
+            }
+        )
+    }
+}
