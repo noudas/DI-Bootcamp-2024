2830fc5d6812f715086be389be5fdb484b3ed5bb
Jonathan K. <noudas1@gmail.com>
2025-01-13 07:14:47 -0300

    Week 11 - Day 1

--- Fullstack_Live/Week_11/Day_1/Lesson/jwt-auth/routes/userRoute.js
+++ Fullstack_Live/Week_11/Day_1/Lesson/jwt-auth/routes/userRoute.js
@@ -1,10 +1,11 @@
 const {Router} = require("express");
 const userController = require("../controllers/userController");
+const {verifyToken} = require("../middlewares/verifyToken")
 
 const router = Router();
 
 router.post("/register", userController.registerUser);
 router.post("/login",userController.loginUser);
-router.get("/all",userController.getUsers);
+router.get("/all",verifyToken, userController.getUsers);
 
 module.exports = router
