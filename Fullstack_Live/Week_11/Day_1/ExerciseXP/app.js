891602501c783b19f14c5c07ef9c899f25f3b84f
Jonathan K. <noudas1@gmail.com>
2025-01-13 17:03:29 -0300

    Week 11 - Day 1

---
+++ Fullstack_Live/Week_11/Day_1/ExerciseXP/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        credentials:true,
+        // origin:['http://localhost:5742']
+    })
+);
+
+const {PORT} = process.env;
+
+app.listen(PORT || 5000, () =>{
+    console.log(`run on ${PORT || 5000}`);
+    
+})
