cdb17c3c171ec4427b1e955e6d4d427fc21823f3
Jonathan K. <noudas1@gmail.com>
2025-01-14 06:35:06 -0300

    Week 11 - Day 2

--- Fullstack_Live/Week_11/Day_2/Lesson/jwt-auth-zwi/Client/src/components/Navbar.jsx
+++ Fullstack_Live/Week_11/Day_2/Lesson/jwt-auth-zwi/Client/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-const Navbar = () =>{
-    return (
-        <>
-            <h1>Navbar</h1>
-        </>
-    )
-}
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-export default Navbar
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(
+        `${apiBaseUrl}/api/user/logout`,
+        {},
+        { withCredentials: true }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <nav>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/login"}>Login</Link>
+        <Link to={"/register"}>Register</Link>
+        <Link to={"/dashboard"}>Dashboard</Link>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
